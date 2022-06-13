5647f8b49a32bafa14be8a4b6c2d13698867490f
manojsdeveloper <manoj.antony32@gmail.com>
2022-06-13 22:08:03 +0530

    JS Reference Type

---
+++ Practice/index.js
@@ -0,0 +1,12 @@
+//reference type
+var object1 = { value: 10 };
+var object2 = object1;
+var object3 = { value: 10 };
+
+//console.log(object1 === object2);
+//console.log(object1 === object3);
+
+//Array are just objects
+console.log([] === [])
+
+//Context vs Scope

8b9cfbc66f03b4adb6cbc60887b54f7f7d068692
manoj.antony32 <manoj.antony32@outlook.com>
2022-06-16 10:32:58 +0530

    ArrayInterviewQuestions: Reverse a string

--- Practice/index.js
+++ Practice/index.js
@@ -7,6 +7,27 @@ var object3 = { value: 10 };
 //console.log(object1 === object3);
 
 //Array are just objects
-console.log([] === [])
+//console.log([] === [])
 
 //Context vs Scope
+
+//reverse a string
+function reverseString(str) {
+  var arr = [];
+  var totalLength = str.length - 1;
+  for(i=totalLength;i>=0;i--) {
+    arr.push(str[i]);
+  }
+  console.log(arr.join(''))
+}
+
+function reverseString2(str) {
+  return str.split('').reverse().join('');
+}
+
+const reverseString3 = str => [...str].reverse().join('');
+
+var result = reverseString3("Hello mano"); //o/p: onam olleH
+console.log(result);
+
+

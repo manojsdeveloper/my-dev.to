ee25e8dfde9c29f4c351c4cda2a7905c0f0052b2
manoj.antony32 <manoj.antony32@outlook.com>
2022-06-20 21:52:10 +0530

    Merge Sorted Array

--- Practice/index.js
+++ Practice/index.js
@@ -30,4 +30,37 @@ const reverseString3 = str => [...str].reverse().join('');
 var result = reverseString3("Hello mano"); //o/p: onam olleH
 console.log(result);
 
+//Merge Sorted Array
 
+function mergeSortedArrays(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  if(array1.length === 0) {
+    return array2;
+  }
+  if(array2.length === 0) {
+    return array1;
+  }
+
+  while(array1Item || array2Item) {
+    console.log(array1Item, array2Item)
+    if(!array2Item | array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+        mergedArray.push(array2Item);
+        array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+
+const result1 = mergeSortedArrays([0,3,4,31], [3,4,6,30]);
+console.log(result1);
